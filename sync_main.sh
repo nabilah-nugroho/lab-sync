8e3b2d06cad6b49e037e01357ebdd478a1549da6
Azeroadrn <daffaadrsyh01@gmail.com>
2026-06-24 12:42:25 +0000

    feat: setup struktur folder dan kerangka menu utama sync_main.sh

--- sync_main.sh
+++ sync_main.sh
@@ -1,1 +1,46 @@
 #daffa
+
+#!/bin/bash
+
+echo "=================================================="
+echo "   SISTEM SINKRONISASI DATA PRAKTIKUM ANTAR LAB"
+echo "=================================================="
+echo "1. Sinkronisasi Otomatis Semua Lab"
+echo "2. Lihat Riwayat Sinkronisasi"
+echo "3. Backup Manual"
+echo "4. Berhentikan Proses Background (Daemon)"
+echo "5. Buat Simulasi File Lab"
+echo "6. Keluar"
+echo "=================================================="
+read -p "Pilih menu [1-6]: " pilihan
+
+case $pilihan in
+    1)
+        echo "Menjalankan sinkronisasi..."
+        # Nanti dipanggil: ./scripts/01_scanner.sh dsb
+        ;;
+    2)
+        echo "Menampilkan riwayat..."
+        # Nanti dipanggil: ./scripts/05_history.sh
+        ;;
+    3)
+        echo "Melakukan backup..."
+        # Nanti dipanggil: ./scripts/03_backup.sh
+        ;;
+    4)
+        echo "Mematikan daemon..."
+        pkill -f sync_daemon.sh
+        echo "Daemon dihentikan."
+        ;;
+    5)
+        echo "Membuat file simulasi..."
+        # Nanti dipanggil: ./scripts/06_simulate.sh
+        ;;
+    6)
+        echo "Keluar dari program."
+        exit 0
+        ;;
+    *)
+        echo "Pilihan tidak valid"
+        ;;
+esac
